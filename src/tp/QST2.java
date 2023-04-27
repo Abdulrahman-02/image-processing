34e28d3ace61fee03af03fda356f0d1c3e38b6ea
paravoke <hjohn6863@gmail.com>
2023-04-27 11:31:32 +0100

    added qst 1.3

---
+++ src/tp/QST2.java
@@ -0,0 +1,37 @@
+package tp;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class QST2 {
+
+	public static void main(String[] args) {
+		try {
+       	 // Charge l'image à partir du fichier
+           BufferedImage blurredImage = ImageIO.read(new File("img.jpg"));
+        // Récupérer les dimensions de l'image
+           int largeur = blurredImage.getWidth();
+           int hauteur = blurredImage.getHeight();
+           
+         
+          
+       
+
+           // Lire les valeurs des pixels de l'image modifiée
+           System.out.println("Valeurs des pixels de l'image modifiée :");
+           for (int y = 0; y < hauteur; y++) {
+               for (int x = 0; x < largeur; x++) {
+                   Color c = new Color(blurredImage.getRGB(x, y));
+                   System.out.print("(" + c.getRed() + "," + c.getGreen() + "," + c.getBlue() + ") ");
+               }
+               System.out.println();
+           }
+
+       } catch (IOException e) {
+           System.out.println("Erreur lors de la lecture de l'image : " + e.getMessage());
+       }
+   }
+}
